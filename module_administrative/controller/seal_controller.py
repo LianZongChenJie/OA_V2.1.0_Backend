df5ffe161c5d52db9ecad779ed6d14ccc51349ec
wangm <wangmaorui0123@163.com>
2026-04-21 10:07:20 +0800

    申请添加创建人

--- module_administrative/controller/seal_controller.py
+++ module_administrative/controller/seal_controller.py
@@ -47,7 +47,9 @@ async def add_seal(
     request: Request,
     query_db: Annotated[AsyncSession, DBSessionDependency()],
     query_object: Annotated[OaSealBaseModel, Body()],
+    current_user: Annotated[CurrentUserModel, CurrentUserDependency()],
 ) -> Response:
+    query_object.admin_id = current_user.user.user_id
     result = await SealService.add_service(query_db, query_object)
     return ResponseUtil.success(msg=result.message)
 
